986760c9c15e86f881729b3311dc77b96aca50b7
ConstanzaBlanco <constanza.blanco@correo.ucu.edu.uy>
2024-09-21 00:30:13 -0300

    Parametros y Metodos definidos

---
+++ src/Program/ICharacter.cs
@@ -0,0 +1,17 @@
+﻿namespace RoleplayGame2;
+
+public interface ICharacter
+{
+    public string name { get; set; }
+    public int HealthLevel { get; set; }
+    public int AttackLevel { get; set; }
+    public int DefenseLevel { get; set; }
+    public bool IsAlive { get; set; }
+    public List<IItems> Inventory { get; set; }
+
+    public void AddItem(IItem item);
+    public void RemoveItem(IItem item);
+    public void Attack(IItem item, ICharacter personaje1);
+    public void Defense(IItem item);
+    public void PrintStatus();
+}
